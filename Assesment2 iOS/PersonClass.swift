019157795d022dff0be3064c4d7330dfc8909d35
Sawyer Cherry <sawyeracherry@gmail.com>
2018-10-17 11:24:55 -0500

    here it is professor

---
+++ Assesment2 iOS/PersonClass.swift
@@ -0,0 +1,22 @@
+//
+//  PersonClass.swift
+//  Assesment2 iOS
+//
+//  Created by Sawyer Cherry on 10/17/18.
+//  Copyright © 2018 Sawyer Cherry. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    var firstName: String
+    var lastName: String
+    var email: String
+    
+    init(firstName: String, lastName: String, email: String) {
+    
+    self.firstName = firstName
+    self.lastName = lastName
+    self.email = email
+    }
+}
--- Assesment2 iOS/ViewController.swift
+++ Assesment2 iOS/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNameTextLabel: UILabel!
+    @IBOutlet weak var lastNameTextLabel: UILabel!
+    @IBOutlet weak var emailTextLabel: UILabel!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        if emailTextField.text == "" {
+            fatalError()
+        }
+        if lastNameTextField.text == ""{
+            fatalError()
+        }
+        if firstNameTextField.text == "" {
+            fatalError()
+        }
+        //put thwe swifty stuff here ipsum fuckit
+        let newPerson = Person(firstName: firstNameTextField.text!, lastName: lastNameTextField.text!, email: emailTextField.text!)
+        
+        let submitAlert = UIAlertController(title: "Person Added", message: "\(newPerson.firstName) \(newPerson.lastName) with email \(newPerson.email) was added to the database.", preferredStyle: .alert)
+        
+        let closeAction = UIAlertAction(title: "Close", style: .default)
+        
+        submitAlert.addAction(closeAction)
+        
+        self.present(submitAlert, animated: true, completion: nil)
+      clearTextFields()
+    }
+    func clearTextFields() {
+        emailTextField.text = ""
+        lastNameTextField.text = ""
+        firstNameTextField.text = ""
+    }
+    
 }
 
